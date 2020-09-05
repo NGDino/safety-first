1b625e429205c750f0f98ea04ab52c7bddf881ea
Neil Dino <N.dino90@gmail.com>
2020-09-05 16:58:24 -0600

    stars generated dynamically

--- public/javascript/safety-rating.js
+++ public/javascript/safety-rating.js
@@ -1,19 +1,19 @@
-// const { json } = require("sequelize/types");
-
-// var rateyo = require("rateyo")
 $(function() {
   console.log( "ready!" );
 
-
-
-
+// get id for fetch
 const id = window.location.toString().split('/')[
   window.location.toString().split('/').length - 1
 ];
 
+//api endpoint
 const endpoint = `/../../api/businesses/${id}`;
 
-const trueValues = [];
+//let trueValues =[]
+let percentArr = []
+let rating 
+
+let trueCount = 0;
 
 fetch(endpoint)
   .then(response => response.json())
@@ -25,42 +25,50 @@ fetch(endpoint)
         // var obj = JSON.parse(bizpost)
         var values = Object.keys(bizpost).map(function (key){return bizpost[key]; });
         console.log('the values', values)
+        var number = []
+        for(i = 2; i < values.length; i++){
+          if (values[i]){
+            number.push(1)
+          } else {
+            number.push(0)
+          }
+        }
+        var eachTrueAmount = number.reduce((a,b)=> a + b ,0)
+        var eachTruePercent = eachTrueAmount / 6
+        percentArr.push(Math.round(eachTruePercent * 100))
+        // Math.round(eachTruePercent)
         
-          var trueAnswers = values.filter(function(e) { return e === true })
-          console.log('these are true',trueAnswers)
-        trueValues.push(trueAnswers)
+          // var trueAnswers = values.filter(function(e) { return e === true }).length
+          // console.log('these are true length', trueAnswers)
+        
           
       }
+      let average = (array) => array.reduce((a, b) => a + b) / array.length;
+      rating = average(percentArr);
+      console.log(' if this works', percentArr)
+      console.log('afkjdlkfjfkj rating', rating)
+      ratingFunc(rating)
     // };
-      console.log('true values', trueValues)
+      // console.log('true values', trueValues)
       // let trueCount= trueValues.joined().count;
       
   })
 
-  
+  console.log('global', rating)
 
 
 
-// const getSafetyScore = (reviews) =>{ 
-
-  
-
-//   for (i = 0; i < reviews.length; i++) {
-//     const contactless = posts[i].contactless_payment;
-//     console.log(contactless)
-    
-//   }
-
-// }
 
 
-
+const ratingFunc =(rating) =>{
 $(function () {
  
   $("#rateYo").rateYo({
-    rating: 3.6
+    rating: `${rating}%`,
+    precision: 0
   });
  
 });
+}
 
 });
